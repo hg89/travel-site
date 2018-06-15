f17fa2b377e1fcb78cd52ca00990a663dc2e829f
Geert <hermans_geert1@hotmail.com>
2018-06-16 00:02:22 +0200

    added basic mobile responsiveness

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ const autoprefixer = require('autoprefixer')
 const cssvars = require('postcss-simple-vars')
 const nested = require('postcss-nested')
 const cssimport = require('postcss-import')
+const mixins = require('postcss-mixins')
 
 gulp.task('styles', function() {
     return gulp
         .src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
         .on('error', function(errorInfo) {
             console.log(errorInfo.toString())
             this.emit('end')
