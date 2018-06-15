5a8b9db94c36026656f3a3b405f69a81efaeac89
Geert <hermans_geert1@hotmail.com>
2018-06-15 20:29:29 +0200

    Improved Gulp functionality

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssvars = require('postcss-simple-vars')
+const nested = require('postcss-nested')
+const cssimport = require('postcss-import')
+
+gulp.task('styles', function() {
+    return gulp
+        .src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+        .on('error', function(errorInfo) {
+            console.log(errorInfo.toString())
+            this.emit('end')
+        })
+        .pipe(gulp.dest('./app/temp/styles'))
+})
