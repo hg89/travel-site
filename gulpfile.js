d452b938e6c8b416674a14b56ae00954f7bada2d
Geert <victurus7@gmail.com>
2018-06-15 16:54:50 +0200

    Rearranged files and added Gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+
+gulp.task('default', function() {
+    console.log('Hooray - you created a Gulp task.')
+})
+
+gulp.task('html', function() {
+    console.log('Imagine something useful being done to your html here')
+})
+
+gulp.task('styles', function() {
+    console.log('Imagine Sass or PostCSS tasks running here')
+})
+
+gulp.task('watch', function() {
+    watch('./app/assets/index.html', function() {
+        gulp.setMaxListeners('html')
+    })
+
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('styles')
+    })
+})
